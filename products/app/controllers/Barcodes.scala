25ebd73704f5ece77ac13645535f5fa36674dde1
Arjay Adong <arjayads@gmail.com>
2016-03-31 09:29:27 +0800

    fix encoding issue

--- products/app/controllers/Barcodes.scala
+++ products/app/controllers/Barcodes.scala
@@ -1,39 +1,36 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
+import play.api.mvc.{Result, Action, Controller}
 
-import play.api.mvc.{Action, Controller}
 
+@Singleton
+class Barcodes @Inject() extends Controller {
 
-@Singleton()
-class Barcodes  @Inject() extends Controller {
-
-  val ImageResolution = 144
-  def barcode(ean: Long) = Action {
-    import java.lang.IllegalArgumentException
-    val MimeType = "image/png"
+	val ImageResolution = 144
+	def barcode(ean: Long) = Action { implicit request =>
+		val MimeType = "image/png"
     try {
       val imageData = ean13BarCode(ean, MimeType)
       Ok(imageData).as(MimeType)
     }
     catch {
-      case e: IllegalArgumentException =>
-        BadRequest("Couldn’t generate bar code. Error: " + e.getMessage)
+      case e: IllegalArgumentException => BadRequest("Couldn't generate bar code. Error: " + e.getMessage)
     }
-  }
+	}
 
-  def ean13BarCode(ean: Long, mimeType: String): Array[Byte] = {
-    import java.io.ByteArrayOutputStream
-    import java.awt.image.BufferedImage
-    import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider
-    import org.krysalis.barcode4j.impl.upcean.EAN13Bean
-    val output: ByteArrayOutputStream = new ByteArrayOutputStream
-    val canvas: BitmapCanvasProvider =
-      new BitmapCanvasProvider(output, mimeType, ImageResolution,
-        BufferedImage.TYPE_BYTE_BINARY, false, 0)
-    val barcode = new EAN13Bean()
-    barcode.generateBarcode(canvas, String valueOf ean)
-    canvas.finish
-    output.toByteArray
-  }
+	def ean13BarCode(ean: Long, mimeType: String): Array[Byte] = {
+		import java.io.ByteArrayOutputStream
+		import java.awt.image.BufferedImage
+		import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider
+		import org.krysalis.barcode4j.impl.upcean.EAN13Bean
+
+		val output: ByteArrayOutputStream = new ByteArrayOutputStream
+		val canvas: BitmapCanvasProvider = new BitmapCanvasProvider(output, mimeType, ImageResolution, BufferedImage.TYPE_BYTE_BINARY, false, 0)
+		val barcode = new EAN13Bean()
+
+		barcode.generateBarcode(canvas, String valueOf ean)
+		canvas.finish
+		output.toByteArray
+	}
 }
